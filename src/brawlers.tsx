f6923dda34cb3704ca496b81c754898389c62703
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-06-24 18:58:12 +0200

    brawler grid size update

--- src/brawlers.tsx
+++ src/brawlers.tsx
@@ -7,30 +7,40 @@ export default function SearchPokemon() {
   const brawlerList = brawlers.items;
 
   return (
-    <Grid throttle searchBarPlaceholder="Search brawler by name">
-      
-        return (
-          <Grid.Section title="brawlers">
-            {brawlerList.map((brawler) => {
-              return (
-                <Grid.Item
-                  key={brawler.id}
-                  //
-                  content={"https://cdn-old.brawlify.com/brawler-bs/"+ (brawler.name =="EL PRIMO" ?"El-Primo":  (brawler.name =="MR. P"?"Mr.P":(brawler.name =="8-BIT"?"8-Bit":(brawler.name =="R-T"?"R-T":brawler.name.charAt(0) + brawler.name.slice(1).toLowerCase()))))+".png"}
-                  title={brawler.name}
-                  subtitle={`#${brawler.id.toString().padStart(3, "0")}`}
-                  keywords={[brawler.id.toString(), brawler.name]}
-                  actions={
-                    <ActionPanel>
-                      <Action.Push title="Show Details" icon={Icon.Sidebar} target={<BrawlerComponent id={""+brawler.id} />} />
-                    </ActionPanel>
-                  }
-                />
-              );
-            })}
-          </Grid.Section>
-        );
-      
+    <Grid throttle searchBarPlaceholder="Search Brawlers By Bame" navigationTitle="Brawlers List"  fit={Grid.Fit.Fill}  columns={10} >
+      <Grid.Section title="Brawlers">
+        {brawlerList.map((brawler) => {
+          return (
+            <Grid.Item
+              key={brawler.id}
+              content={
+                "https://cdn-old.brawlify.com/brawler-bs/" +
+                (brawler.name == "EL PRIMO"
+                  ? "El-Primo"
+                  : brawler.name == "MR. P"
+                  ? "Mr.P"
+                  : brawler.name == "8-BIT"
+                  ? "8-Bit"
+                  : brawler.name == "R-T"
+                  ? "R-T"
+                  : brawler.name.charAt(0) + brawler.name.slice(1).toLowerCase()) +
+                ".png"
+              }
+              title={brawler.name}
+              keywords={[brawler.id.toString(), brawler.name]}
+              actions={
+                <ActionPanel>
+                  <Action.Push
+                    title="Show Details"
+                    icon={Icon.Sidebar}
+                    target={<BrawlerComponent id={"" + brawler.id} />}
+                  />
+                </ActionPanel>
+              }
+            />
+          );
+        })}
+      </Grid.Section>
     </Grid>
   );
 }
